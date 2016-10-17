ab6e0152f6e0db96f12f6aa5366eb8f3ffcfb237
[John Patterson] <pattersonjc1@gmail.com>
2016-10-17 13:23:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TIY-4-1-Soundcloud-App
+
+The Iron Yard week 4, day 1 assignment.  The goal of this assignment was to build a basic app to play back SoundCloud files using the Sound Cloud API and the HTML 5 audio player.
---
+++ js/main.js
@@ -0,0 +1,30 @@
+jQuery(function() {
+
+    // $("#submitForm").on("submit", function() {
+    //     event.preventDefault();
+    //     var name = $("#nameBox").val();
+    //     var meal = $("#textBox").val();
+    //     var restaurant = $("#restBox").val();
+    //     getName(name, meal, restaurant);
+
+    // })
+    // $("#button").on("click", function() {
+    //     var name = $("#nameBox").val();
+    //     var meal = $("#textBox").val();
+    //     var restaurant = $("#restBox").val();
+    //     getName(name, meal, restaurant);
+
+    // })
+
+
+    // function getName(name, meal, restaurant) {
+    //     $.ajax({
+    //         method: 'POST',
+    //         data: { name: name, meal: meal, restaurant: restaurant },
+    //         url: "http://192.168.1.8:3005/doSomething",
+    //         success: function(result) {},
+    //     });
+    // }
+
+
+});
